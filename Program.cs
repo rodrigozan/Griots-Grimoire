d4438cfba84b8350e372ab95ce6996f75652294f
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-06-11 11:51:13 -0300

    Add and configuration MongoDB driver in API

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IMongoClient>(provider =>
+{
+    var configuration = provider.GetRequiredService<IConfiguration>();
+    return new MongoClient(configuration.GetSection("MongoDB:ConnectionString").Value);
+});
+
+builder.Services.AddScoped<IMongoDatabase>(provider =>
+{
+    var configuration = provider.GetRequiredService<IConfiguration>();
+    var mongoClient = provider.GetRequiredService<IMongoClient>();
+    return mongoClient.GetDatabase(configuration.GetSection("MongoDB:DatabaseName").Value);
+});
 
 var app = builder.Build();
 
