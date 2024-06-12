b1df39e40d9edbb344ce65dc1b4849a60c4490d4
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-06-12 10:04:18 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,10 +3,7 @@ using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -25,7 +22,6 @@ builder.Services.AddScoped<IMongoDatabase>(provider =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
